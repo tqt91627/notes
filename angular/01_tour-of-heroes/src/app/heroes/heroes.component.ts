ec4b75ae63d4322f12ea8b3c2248824f89a1f94f
tqt91627 <tqt91627@aol.com>
2018-08-11 19:38:33 -0400

    selectable hero list displays selectedHero details

--- angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,6 +10,7 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   
   heroes = HEROES;
+  selectedHero: Hero;
 
   hero: Hero = {
     id: 1,
@@ -19,6 +20,10 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
