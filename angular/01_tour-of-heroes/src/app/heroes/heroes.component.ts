9f227932f45546b91ea9e20c8420aa4d058c9682
tqt91627 <tqt91627@aol.com>
2018-08-13 13:11:21 -0400

    updated heroes component to use hero service

--- angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +11,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   hero: Hero = {
@@ -17,13 +19,18 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
