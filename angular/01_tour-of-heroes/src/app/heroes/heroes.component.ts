283e0310c0dc4344b433385ee509b304f7ef988b
tqt91627 <tqt91627@aol.com>
2018-08-13 13:24:18 -0400

    updated hero service to return observable heroes, updated heroes component to subscribe to the newly return observable

--- angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/01_tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -30,7 +30,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(obsResult => this.heroes = obsResult);
   }
 
 }
