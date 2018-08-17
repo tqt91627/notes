61c7105c860c80cb5fa8439989245604bc435896
tqt91627 <tqt91627@aol.com>
2018-08-17 12:48:30 -0400

    added default route

--- angular/01_tour-of-heroes/src/app/app-routing.module.ts
+++ angular/01_tour-of-heroes/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ]
