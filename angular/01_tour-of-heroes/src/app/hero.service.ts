c61e6a1ae8b83880a6d14ea7db9f837fd767b2e1
tqt91627 <tqt91627@aol.com>
2018-08-21 13:43:13 -0400

    tapping into Observables

--- angular/01_tour-of-heroes/src/app/hero.service.ts
+++ angular/01_tour-of-heroes/src/app/hero.service.ts
@@ -20,17 +20,16 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.log('fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log('fetched heroes')),
       catchError(this.handleError('getHeroes', []))
     );
   }
 
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`
-    // TODO: send the message _after_ fetching the hero
-    this.log(`fetched hero id=${id}`);
     return this.http.get<Hero>(url).pipe(
+      tap(hero => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
