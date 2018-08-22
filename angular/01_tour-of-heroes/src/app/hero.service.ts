cdeafddb79951961234f63c2cef2afa3060a1c7d
tqt91627 <tqt91627@aol.com>
2018-08-22 10:28:44 -0400

    added documentation comments, refactored code

--- angular/01_tour-of-heroes/src/app/hero.service.ts
+++ angular/01_tour-of-heroes/src/app/hero.service.ts
@@ -26,7 +26,7 @@ export class HeroService {
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
-      tap(heroes => this.log('fetched heroes')),
+      tap((heroes: Hero[]) => this.log('getHeroes => fetched heroes')),
       catchError(this.handleError<any>('getHeroes', []))
     );
   }
@@ -35,21 +35,23 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`
     return this.http.get<Hero>(url).pipe(
-      tap(hero => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
+      tap((hero: Hero) => this.log(`getHero => fetched hero id=${hero.id}`)),
+      catchError(this.handleError<Hero>(`getHero => id=${id}`))
     );
   }
 
+  /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      tap(_ => this.log(`updateHero => updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     )
   }
 
+  /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      tap((hero: Hero) => this.log(`addHero => added hero id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
   }
