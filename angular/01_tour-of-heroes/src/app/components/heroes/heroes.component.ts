62649f72cb22d6b72d030e187dff360dc4ad8e66
tqt91627 <tqt91627@aol.com>
2018-09-27 18:06:53 -0400

    introduced scss, added Gingham font, refactored css, fixed autocomplete heroes detail route

--- angular/01_tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular/01_tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from '../../services/hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
